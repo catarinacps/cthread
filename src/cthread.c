864922ca4be0baa40696cafddbdac7726e2b9782
Henrique Silva <henrique.p.silva@outlook.com>
2017-04-30 20:16:43 -0300

    wtf quem cuida do termino de threadddds

--- src/cthread.c
+++ src/cthread.c
@@ -32,10 +32,14 @@ int csetprio (int tid, int prio){
     csetprio altera a prioridade da thread com o identificador tid para a nova prioridade prio. Obs: thread que chamou não perde a CPU, ou seja, continua em executando.
 
     -->lógica:
-    if (procuraNasFilas(filaProcessos, tid)){
+    if (procuraNasFilas(filaProcessos, tid) && testaSeDerRuim(tid, prio)){
         copiaNodo(?);
         destroiNodo(?);
         appendNodoNaFilaNovaPossivelmente();
+
+        return 0;
+    } else{
+        return -90000;
     }
     */
 }
@@ -44,37 +48,86 @@ int cyield (void){
     cyield salva o contexto da thread em execução, altera seu estado, a coloca em apto e pega a próxima thread na lista de aptos, de acordo com a prioridade da mesma.
 
     -->lógica:
-    TCB.context = getcontext();
-    ''.state = apto;
-    pegaProximoEColocaEmExecuzao();
+    if (!filaVazia(aptos)){
+        TCB.context = getcontext();
+        TCB.state = apto;
+        pegaProximoEColocaEmExecuzao();
+
+        return 1;
+    } else{
+        return -9000;
+    }
     */
 }
 int cjoin (int tid){
     /*
-    cjoin, primeiramente, checa se a thread especificada já está sendo esperada, o que indica que essa thread não pode ser espera. Caso contrário, cjoin adiciona a thread indicada e a thread que chamou a função para a lista de esperando????. Além disso, a thread que chamou vai para o estado bloqueado, esperando o término da thread em questão.
+    cjoin, primeiramente, checa se a thread especificada já está sendo esperada, o que indica que essa thread não pode ser esperada. Caso contrário, cjoin adiciona a thread indicada e a thread que chamou a função para a lista de esperando????. Além disso, a thread que chamou vai para o estado bloqueado, esperando o término da thread em questão.
+
+    -->lógica:
+    if (!estaNaFila(tid, esperando)){
+        TCBexec.state = bloqueado;
+        TCBexec.context = getcontext();
+
+        appendFila(tid, esperando);
+
+        return 0;
+    } else{
+        return -8000;
+    }
     */
 }
 int csem_init (csem_t *sem, int count){
     /*
-    memes
-    haha
+    csem_init inicializa uma variável de semáforo e seu respectivo contador.
+
+    ???????????????????????????????????????????????????????????????????????????
+    like, wtf
     */
 }
 int cwait (csem_t *sem){
     /*
-    memes
-    haha
+    cwait recebe um semáforo e checa se o contador deste semáforo é menor ou igual a zero. Se for, o processo é posto em bloqueado, esperando para acessar o recurso controlado pela variavel de semáforo. Caso contrário, o semáforo terá sua variável count decrementada e a thread poderá acessar o recurso controlado.
+
+    -->lógica:
+    república if (sem.count > 0){
+        sem.count--;
+        return 0;
+    } else{
+        TCBexec.state = bloqueado;
+        semBloqueia(sem, TCBexec.tid);
+
+        return -50000;
+    }
     */
 }
 int csignal (csem_t *sem){
     /*
-    memes
-    haha
+    csignal recebe um semáforo e incrementa sua variável de count, indicando que a thread terminou de usar o recurso controlado pelo semáforo. Nesse momento, se houver threads na lista de bloqueados do semáforo, é utilizado a ideia de FIFO, e a primeira thread na fila é posta em apto.
+
+    -->lógica:
+    if (sem.count <= 0){
+        sem.count++;
+
+        tidDesbl = semDesbloqueia(sem); //é o primeiro da fila
+        TCBaux = tiraFila(tid, bloqSem); //bloqSem é a fila de bloqueados por semáforo
+        appenddFila(aptos?, TCBaux);
+
+        return 0;
+    } else{
+        return -900;
+    }
     */
 }
 int cidentify (char *name, int size){
     /*
-    memes
-    haha
+    Escreve nossos nomes em *name se size >= sizeOf(nomes)
+
+    if (sizeOf(nossosNomes) <= size){
+        strcpy(name, ponteiroApontandoParaNossosNomes);
+
+        return 0;
+    } else{
+        return -70000;
+    }
     */
 }
