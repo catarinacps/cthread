1a0a2e6a900ad46a2dfa1703acc4f8a259f1b80f
DESKTOP-EMFP98N\Gabriel <44637g@gmail.com>
2017-05-09 20:47:04 -0300

    lib com L_init, L_insert e L_vazia

--- src/auxlib.c
+++ src/auxlib.c
@@ -2,4 +2,54 @@
 #include "support.h"
 #include "cdata.h"
 
+struct	nodoLista {
+	void 	*nodo;			// Ponteiro para os dados do NODO
+	struct	nodoLista *ant;	// Ponteiro para o nodo anterior
+	struct	nodoLista *prox;	// Ponteiro para o nodo posterior
+};
+typedef struct nodoLista	LISTA;
+
+//inicia lista
+LISTA* L_init(){
+	return NULL;
+}
+
+//insere na lista e retorna posição
+int L_insert(LISTA *lista, void *dado){
+	LISTA add*, *nodo;
+	int iterator=0;
+	
+	nodo=lista;
+	
+	add=malloc(sizeof(LISTA));
+	add.nodo=dado;
+	add.ant=NULL;
+	add.prox=NULL;
+	
+	if(L_vazia(lista)==1){
+		*lista=add;
+		return 0;
+	}
+	else{
+		do{
+		nodo=(*nodo).prox;
+		i++;
+		}
+		while(L_vazia(nodo)==0);
+		
+		(*nodo).prox=add;
+		(*add).ant=nodo;
+		
+		return i;
+	}
+}
+
+//retorna 1 se a lista esta vaiza, senao retorna 0
+int L_vazia(LISTA *lista){
+	if(lista==NULL)
+		return 1;
+	else return 0;
+}
+
+
 
