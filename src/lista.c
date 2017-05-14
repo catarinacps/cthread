878f583a2143f72fe6f839323abe268b5df71341
Henrique Silva <henrique.p.silva@outlook.com>
2017-05-13 21:30:45 -0300

    tem um lista.h pq ele tava reclamando de declaracao implicita btw n sei usar include guards rip

--- src/lista.c
+++ src/lista.c
@@ -1,22 +1,12 @@
-#include <stdlib.h>
-
-struct nodoLista
-{
-    void *dados;            // Ponteiro para os dados do NODO
-    struct nodoLista *ant;  // Ponteiro para o nodo anterior
-    struct nodoLista *prox; // Ponteiro para o nodo posterior
-};
-typedef struct nodoLista LISTA;
+#include "../include/lista.h"
 
 // inicia lista
-LISTA * initLista()
-{
+LISTA *initLista() {
     return NULL;
 }
 
-// insere no fim da lista e retorna posição
-LISTA* insertLista(LISTA *lista, void *dado)
-{
+// insere no fim da lista e retorna posiï¿½ï¿½o
+LISTA *insertLista(LISTA *lista, void *dado) {
     LISTA *add, *nodo;
     int iterator = 0;
 
@@ -28,14 +18,10 @@ LISTA* insertLista(LISTA *lista, void *dado)
     add->ant = NULL;
     add->prox = NULL;
 
-    if (emptyLista(lista) == 1)   // se a lista estiver vazia coloca no inicio
-    {
+    if (emptyLista(lista) == 1) { // se a lista estiver vazia coloca no inicio
         return add;
-    }
-    else
-    {
-        while (nodo->prox != NULL)   // senao acha o fim
-        {
+    } else {
+        while (nodo->prox != NULL) { // senao acha o fim
             nodo = nodo->prox;
             iterator++;
         }
@@ -48,112 +34,85 @@ LISTA* insertLista(LISTA *lista, void *dado)
 }
 
 // retorna 1 se a lista esta vazia, senao retorna 0
-int emptyLista(LISTA *lista)
-{
-    if (lista == NULL)
-    {
+int emptyLista(LISTA *lista) {
+    if (lista == NULL) {
         return 1;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
 
 // remove da lista noda na posicao pos, retorna 1 se conseguiu, senao retorna 0
-LISTA* removeLista(LISTA *lista, int pos)
-{
+LISTA *removeLista(LISTA *lista, int pos) {
     LISTA *nodo;
     nodo = lista;
     int i;
 
-    if (emptyLista(lista) == 0)
-    {
-        if(pos==0)
-        {
-            nodo=nodo->prox;
+    if (emptyLista(lista) == 0) {
+        if (pos == 0) {
+            nodo = nodo->prox;
             free(nodo->ant);
             return nodo;
-        }
-        else
-        {
-            for (i = 0; i < pos; i++)
-            {
-                if (nodo->prox == NULL)
-                {
+        } else {
+            for (i = 0; i < pos; i++) {
+                if (nodo->prox == NULL) {
                     return lista;
                 } // fim da lista -> nao ha elemento pos na lista
-                else
-                {
+                else {
                     nodo = nodo->prox;
                 } // pula pro proximos ate chegar em pos
             }
 
             nodo->ant->prox = nodo->prox;
-            if(nodo->prox!=NULL)
-                nodo->prox->ant= nodo->ant; // encadeia nodos vizinhos
+            if (nodo->prox != NULL)
+                nodo->prox->ant = nodo->ant; // encadeia nodos vizinhos
             free(nodo);
             return lista;
         }
-    }
-    else
-    {
+    } else {
         return lista;
     } // lista vazia -> nao ha oq remover
 }
 
 // retorna o dado da lista, senao retorna NULL
-void * getNodeLista(LISTA *lista, int pos)
-{
+void *getNodeLista(LISTA *lista, int pos) {
     LISTA *nodo;
     nodo = lista;
     int i;
 
-    if (emptyLista(lista) == 0)
-    {
-        for (i = 0; i < pos; i++)
-        {
-            if (nodo->prox == NULL)
-            {
+    if (emptyLista(lista) == 0) {
+        for (i = 0; i < pos; i++) {
+            if (nodo->prox == NULL) {
                 return NULL;
             } // fim da lista -> nao ha elemento pos na lista
-            else
-            {
+            else {
                 nodo = nodo->prox;
             } // pula pro proximos ate chegar em pos
         }
         return nodo->dados;
-    }
-    else
-    {
+    } else {
         return NULL;
     }
-
 }
 // retorna o proximo nodo
-LISTA * getNextNodeLista(LISTA *lista)
-{
+LISTA *getNextNodeLista(LISTA *lista) {
     return lista->prox;
 }
 
 // retorna o nodo anterior
-LISTA * getPrevNodeLista(LISTA *lista)
-{
+LISTA *getPrevNodeLista(LISTA *lista) {
     return lista->ant;
 }
 
 // retorna o primeiro nodo da lista, senao retorna NULL
-LISTA * getFirstNodeLista(LISTA *lista)
-{
+LISTA *getFirstNodeLista(LISTA *lista) {
     LISTA *nodo;
     nodo = lista;
 
     if (emptyLista(nodo) == 1)
         return NULL;
-    else
-    {
-        while (nodo->ant != NULL)
-        {
+    else {
+        while (nodo->ant != NULL) {
             nodo = nodo->ant;
         }
         return nodo;
@@ -161,19 +120,14 @@ LISTA * getFirstNodeLista(LISTA *lista)
 }
 
 // retorna o ultimo nodo da lista, senao retorna NULL
-LISTA * getLastNodeLista(LISTA *lista)
-{
+LISTA *getLastNodeLista(LISTA *lista) {
     LISTA *nodo;
     nodo = lista;
 
-    if (emptyLista(nodo) == 1)
-    {
+    if (emptyLista(nodo) == 1) {
         return NULL;
-    }
-    else
-    {
-        while (nodo->prox != NULL)
-        {
+    } else {
+        while (nodo->prox != NULL) {
             nodo = nodo->prox;
         }
         return nodo;
@@ -181,16 +135,13 @@ LISTA * getLastNodeLista(LISTA *lista)
 }
 
 // destroi a lista
-void destroyLista(LISTA *lista)
-{
+void destroyLista(LISTA *lista) {
     LISTA *nodo;
     nodo = lista;
 
-    while (nodo->prox != NULL)
-    {
+    while (nodo->prox != NULL) {
         nodo = nodo->prox;
         free(nodo->ant);
     }
     free(nodo);
 }
-
