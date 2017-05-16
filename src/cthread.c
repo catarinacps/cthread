2c197cd42d8b2e0fa1db3d7a02aba23393126484
Henrique Silva <henrique.p.silva@outlook.com>
2017-05-15 22:35:49 -0300

    COMMIT DO BEAUTIFY
    obs: mto boa parabens a duplinha do barulho
    vamo testar tudo amanhã

--- src/cthread.c
+++ src/cthread.c
@@ -2,8 +2,7 @@
 #include "../include/auxlib.h"
 #include <string.h>
 
-
-int ccreate (void* (*start)(void*), void *arg, int prio) {
+int ccreate(void *(*start)(void *), void *arg, int prio) {
     TCB_t *tcbMain, *tcbNew;
     char mainStack[SIGSTKSZ], newStack[SIGSTKSZ];
 
@@ -22,7 +21,8 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
 
         setTidExec(tcbMain->tid);
 
-        makecontext(tcbMain->context.uc_link, (void (*)(void)) dispatcher, 1, tcbMain->tid);
+        makecontext(tcbMain->context.uc_link, (void (*)(void))dispatcher, 1,
+                    tcbMain->tid);
         tcbMain->context.uc_stack.ss_sp = mainStack;
         tcbMain->context.uc_stack.ss_size = sizeof(mainStack);
 
@@ -34,9 +34,10 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
     tcbNew->tid = getNextTid();
     tcbNew->state = 1;
     tcbNew->ticket = prio;
-    makecontext(&(tcbNew->context), (void (*)(void)) start, 1, arg);
+    makecontext(&(tcbNew->context), (void (*)(void))start, 1, arg);
 
-    makecontext(tcbNew->context.uc_link, (void (*)(void)) dispatcher, 1, tcbNew->tid);
+    makecontext(tcbNew->context.uc_link, (void (*)(void))dispatcher, 1,
+                tcbNew->tid);
     tcbNew->context.uc_stack.ss_sp = newStack;
     tcbNew->context.uc_stack.ss_size = sizeof(newStack);
 
@@ -45,8 +46,8 @@ int ccreate (void* (*start)(void*), void *arg, int prio) {
     return tcbNew->tid;
 }
 
-int csetprio (int tid, int prio) {
-	TCB_t *tcbAux;
+int csetprio(int tid, int prio) {
+    TCB_t *tcbAux;
 
     if (prio < 0 || prio > 4) {
         return -1;
@@ -63,14 +64,14 @@ int csetprio (int tid, int prio) {
     }
 }
 
-int cyield (void) {
+int cyield(void) {
     int *ptTidExec;
     TCB_t *tcbExec;
 
     if (emptyTCB()) {
         return -1;
     } else {
-		ptTidExec=malloc(sizeof(int));
+        ptTidExec = malloc(sizeof(int));
         *ptTidExec = tidExec;
 
         tcbExec = findTCB(tidExec);
@@ -80,7 +81,7 @@ int cyield (void) {
         } else {
             return -1;
         }
-		
+
         if (tcbExec->state == 1) {
             dispatcher(-1);
         }
@@ -88,129 +89,133 @@ int cyield (void) {
     }
 }
 
-int cjoin (int tid) {
+int cjoin(int tid) {
     TCB_t *tcbAux;
-	LISTA *nodo;
-	ESPERA *aux;
-	
-	nodo=esperando;
-	
-	if(tcbAux=findTCB(tid)){
-		return -1;
-	}
-	if(tcbAux->state==4){
-		return -1;
-	}
-	
-	if(emptylista(esperando)){
-		//so botar
-		tcbAux=findTCB(tidExec);
-		tcbAux->state=3;
-		
-		aux=malloc(sizeof(ESPERA));
-		aux->tidEsperado=tid;
-		aux->tidBloqueado=tidExec;
-		esperando=insertLista(esperando,(void*)aux);
-		
-		getcontext(&(tcbAux->context));
-		
-		
-	}else{
-		
-		do{
-			aux = (ESPERA *)nodo->dados;
-			nodo = getNextNodeLista(esperando);
-		}while (aux->tidEsperado != tid && nodo != NULL);
-		
-		if(nodo==NULL){
-			//so botar
-			tcbAux=findTCB(tidExec);
-			tcbAux->state=3;
-			
-			aux=malloc(sizeof(ESPERA));
-			aux->tidEsperado=tid;
-			aux->tidBloqueado=tidExec;
-			esperando=insertLista(esperando,(void*)aux);
-		
-			getcontext(&(tcbAux->context));
-		}else{
-			return -1;
-		}
-	}
-	if (tcbAux->state == 3) {
-            dispatcher(-1);
-        }
-	return 	0;
-}
-int csem_init (csem_t *sem, int count){
-    *sem->count=count;
-	//add lista de semaforos da auxlib?
-	if(CreateFila2(*sem->fila)!=0)
-		return -1;
-	else return 0;
-}
-int cwait (csem_t *sem){
-	TCB_t tcbAux;
-	int *tidBloqueado;
-	
-	if(sem == NULL)
-		return -1;
-	*sem->count--;
-	if(*sem->count>-1){	//gambiarra
-		return 0;
-	}else{
-		tcbAux=findTCB(tidExec); 
-		tcbAux->state=3;
-		
-		tidBloqueado=malloc(sizeof(int));
-		*tidBloqueado=tidExec;
-		
-		if(AppendFila2(*sem->fila,(void*)tidBloqueado)!=0)
-			return -1;
-		
-		getcontext(&(tcbAux->context));
-		
-		if (tcbAux->state == 3) {
-            dispatcher(-1);
-        }
-		return 	0;
-	}	
-}
-int csignal (csem_t *sem){
-	int tidBloqueado, *tidApto;
-	TCB_t *tcbApto;
-	
-	if(sem==NULL)
-		return -1;
-	*sem->count++;
-	if(FirstFila2(*sem->fila)!=0){
-		return 0;
-	}else{
-		tidBloqueado= *((int*)GetAtIteratorFila2(*sem->fila));
-		DeleteAtIteratorFila2(*sem->fila);
-		
-		tcbApto = findTCB(tidBloqueado);
-		tidApto=malloc(sizeof(int));
-		*tidApto=tidBloqueado;
-		
-        if (AppendFila2(aptos[tcbApto->ticket], (void *)tidApto) == 0) {
-            tcbApto->state = 1;
-			return 0;
+    LISTA *nodo;
+    ESPERA *aux;
+
+    nodo = esperando;
+
+    if (tcbAux = findTCB(tid)) {
+        return -1;
+    }
+    if (tcbAux->state == 4) {
+        return -1;
+    }
+
+    if (emptylista(esperando)) {
+        // so botar
+        tcbAux = findTCB(tidExec);
+        tcbAux->state = 3;
+
+        aux = malloc(sizeof(ESPERA));
+        aux->tidEsperado = tid;
+        aux->tidBloqueado = tidExec;
+        esperando = insertLista(esperando, (void *)aux);
+
+        getcontext(&(tcbAux->context));
+
+    } else {
+
+        do {
+            aux = (ESPERA *)nodo->dados;
+            nodo = getNextNodeLista(esperando);
+        } while (aux->tidEsperado != tid && nodo != NULL);
+
+        if (nodo == NULL) {
+            // so botar
+            tcbAux = findTCB(tidExec);
+            tcbAux->state = 3;
+
+            aux = malloc(sizeof(ESPERA));
+            aux->tidEsperado = tid;
+            aux->tidBloqueado = tidExec;
+            esperando = insertLista(esperando, (void *)aux);
+
+            getcontext(&(tcbAux->context));
         } else {
             return -1;
         }
-		
-	}
+    }
+    if (tcbAux->state == 3) {
+        dispatcher(-1);
+    }
+    return 0;
 }
-int cidentify (char *name, int size){
-	char nomes[120];
-	strcpy(nomes,   "Gabriel Stefaniak Niemiec 262503\n"
-					"Henrique Correa Pereira da Silva 262508\n"
-					"Nicolas Eymael da Silva 262506\n");
-	if(strlen(nomes)<=size){
-		strcpy(name,nomes);
-		return 0;
-	}else{
-		return -1;
-	}
+int csem_init(csem_t *sem, int count) {
+    *sem->count = count;
+    // add lista de semaforos da auxlib?
+    if (CreateFila2(*sem->fila) != 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+int cwait(csem_t *sem) {
+    TCB_t tcbAux;
+    int *tidBloqueado;
+
+    if (sem == NULL) {
+        return -1;
+    }
+
+    *sem->count--;
+    if (*sem->count > -1) { // gambiarra
+        return 0;
+    } else {
+        tcbAux = findTCB(tidExec);
+        tcbAux->state = 3;
+
+        tidBloqueado = malloc(sizeof(int));
+        *tidBloqueado = tidExec;
+
+        if (AppendFila2(*sem->fila, (void *)tidBloqueado) != 0) {
+            return -1;
+        }
+
+        getcontext(&(tcbAux->context));
+
+        if (tcbAux->state == 3) {
+            dispatcher(-1);
+        }
+        return 0;
+    }
+}
+int csignal(csem_t *sem) {
+    int tidBloqueado, *tidApto;
+    TCB_t *tcbApto;
+
+    if (sem == NULL) {
+        return -1;
+    }
+    *sem->count++;
+    if (FirstFila2(*sem->fila) != 0) {
+        return 0;
+    } else {
+        tidBloqueado = *((int *)GetAtIteratorFila2(*sem->fila));
+        DeleteAtIteratorFila2(*sem->fila);
+
+        tcbApto = findTCB(tidBloqueado);
+        tidApto = malloc(sizeof(int));
+        *tidApto = tidBloqueado;
+
+        if (AppendFila2(aptos[tcbApto->ticket], (void *)tidApto) == 0) {
+            tcbApto->state = 1;
+            return 0;
+        } else {
+            return -1;
+        }
+    }
+}
+int cidentify(char *name, int size) {
+    char nomes[120];
+    strcpy(nomes, "Gabriel Stefaniak Niemiec 262503\n"
+                  "Henrique Correa Pereira da Silva 262508\n"
+                  "Nicolas Eymael da Silva 262506\n");
+    if (strlen(nomes) <= size) {
+        strcpy(name, nomes);
+        return 0;
+    } else {
+        return -1;
+    }
 }
